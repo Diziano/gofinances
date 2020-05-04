ed9750b70169f00aad55ef3d8584f4b4e987c1de
Diziano <dizianomachado@gmail.com>
2020-05-03 21:52:50 -0300

    Changed configs

--- backend/src/database/index.ts
+++ backend/src/database/index.ts
@@ -8,7 +8,7 @@ export default async (name = 'default'): Promise<Connection> => {
       name,
       database:
         process.env.NODE_ENV === 'test'
-          ? 'gostack_desafio06_tests'
+          ? 'gofinances_tests'
           : defaultOptions.database,
     }),
   );
